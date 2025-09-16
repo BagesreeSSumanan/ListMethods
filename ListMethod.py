18bb590f8e5102883ef0b91e3d62ab31046c5548
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-09-16 17:57:33 +0530

    list method

--- ListMethod.py
+++ ListMethod.py
@@ -8,3 +8,27 @@ for a, b in zip(list1, list2):
 languages = ['Java', 'Python', 'JavaScript']
 languages[1] = ".Net"
 print(languages)
+
+# 3.Remove all occurrences of a specific item from a list.
+languages = ['Java', 'Python', 'JavaScript','Python']
+languages = [lang for lang in languages if lang != "Python"]
+print(languages)
+
+# 4.Given a nested list ['a', ['bb', 'cc'], 'd']Insert item ‘dd’ to the second sublist’s first position so that resulting list will be['a', [‘dd’,'bb', 'cc'], 'd']
+nested_list = ['a', ['bb', 'cc'], 'd']
+nested_list[1].insert(0, 'dd')
+print(nested_list)
+
+# 5.Iterate through the following nested list using for loop and and print the elements
+L = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+
+# 6.Given a list [10,23,40,[9,3,4],6,7].Print the first 2 elements of sublist at position 3
+Newlist = [10,23,40,[9,3,4],6,7]
+Newlist = Newlist[3][:2]
+Newlist=Newlist[:2]
+print(Newlist)
+
+#7.There are 5 students in this class whose names and grades are assembled to build the following list, list the name and grade 
+Input = [['rishav', 10], ['akash', 50], ['ram', 80], ['gaurav', 85],['Akhil', 58]]
+for student in Input:
+    print("Name:", student[0], "Grade:", student[1])
